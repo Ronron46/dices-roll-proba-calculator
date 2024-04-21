10a15d62302ba6459e599905b837efbd39f93a75
Ronron <ronflettemaxime@gmail.com>
2024-04-21 16:32:35 +0200

    added time module for comparisons between branches

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from objects.proba import Calc_proba
 from objects.graphe import Graphe
 import matplotlib.pyplot as plt
 from objects.data_cleaner import DataCleaner
+import time
 
 
 dice_list=input('entrer la liste de dé à lancer, si vous voulez l\'avantage, ajouter un "a" collé au nombre par une virgule, pour un désavantage, mettez un "d". Exemple: 4,a 3 6 12,d...')
+time_start=time.time()
 clean_dice_list=DataCleaner(dice_list).cleaning()
 bucket=DiceBucket(clean_dice_list)
 tab=Counter(bucket)
+print("ça a prit", time.time() - time_start, " secondes")
 target=input('quelle est la valeur cible?')
 print("Vous avez ", Calc_proba(tab, int(target)).proba()*100, "% de chance de réussir")
 print('Vous avez ', Calc_proba(tab,int(target)).proba_strict_equal()*100, '% de chance d\'obtenir exactement', target)
